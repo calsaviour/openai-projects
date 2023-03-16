38b7b2997f0c89e7d3ad490274df4baecd45e71d
Calvin Low <calsaviour@gmail.com>
2023-03-16 20:47:50 +0800

    WIP side bar

--- sidebar/src/components/Sidebar.tsx
+++ sidebar/src/components/Sidebar.tsx
@@ -1,12 +1,44 @@
 import Image from "next/image";
 import Link from "next/link";
 import { MdDashboard } from "react-icons/md"
+import { AiOutlineHome } from "react-icons/ai";
+import { BsPeople } from "react-icons/bs";
+import { TiContacts } from "react-icons/ti";
+import { FiMail } from "react-icons/fi";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { useRouter } from "next/router";
+
+
+const sidebarItems = [
+    {
+      name: "Home",
+      href: "/",
+      icon: AiOutlineHome,
+    },
+    {
+      name: "About",
+      href: "/about",
+      icon: BsPeople,
+    },
+    {
+      name: "Mails",
+      href: "/mails",
+      icon: FiMail,
+    },
+    {
+      name: "Contact",
+      href: "/contact",
+      icon: TiContacts,
+    },
+  ];
 
 export default function Sidebar() {
+    const router = useRouter();
+
     return (
         <div>
             <aside className="sidebar">
-                <div>
+                <div className="sidebar__top">
                     <Image
                         alt="logo"
                         src="/logo.jpg"
@@ -16,16 +48,29 @@ export default function Sidebar() {
                     />
                 </div>
                 <ul className="sidebar__list">
-                    <li className="sidebar__item">
-                        <Link href="/" className="sidebar__link">
-                            <div className="sidebar_icon">
-                                <MdDashboard />
-                            </div>
-                                <span>
-                                    Dashboard
+                    {sidebarItems.map( ({name, href, icon: Icon})=> {
+                        return (
+                            <li className="sidebar__item" key={name}>
+                                <Link
+                                    className={`sidebar__link ${
+                                        router.pathname == href ? "sidebar__link--active" : ""
+                                    }`}
+                                    href={href}
+                                >
+                                <span
+                                    className="sidebar__icon"
+                                >
+                                    <Icon />
                                 </span>
-                        </Link>
-                    </li>
+                                <span
+                                    className="sidebar__name"
+                                >
+                                    {name}
+                                </span>
+                                </Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </aside>
         </div>
