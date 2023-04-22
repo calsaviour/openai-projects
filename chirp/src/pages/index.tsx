e36a18701364e4615b4290bb31bf466c951cd0d7
Calvin Low <calsaviour@gmail.com>
2023-04-22 16:45:21 +0800

    Add loading component

--- chirp/src/pages/index.tsx
+++ chirp/src/pages/index.tsx
@@ -1,8 +1,13 @@
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-
 import { api, RouterOutputs } from "~/utils/api";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
+
+dayjs.extend(relativeTime); 
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -11,9 +16,11 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex gap-3 w-full">
-      <img src={user.profileImageUrl}
+      <Image  src={user.profileImageUrl}
             alt="Profile image"
             className="h-14 w-14 rounded-full"
+            width={56}
+            height={56}
       />
       <input placeholder="Type some emojis" 
             className="grow bg-transparent outline-none"/>
@@ -27,11 +34,20 @@ const PostView = (props: PostWithUser) => {
   const {post, author} = props;
   return (
     <div key={post.id} className="flex border-b border-slate-400 p-4 gap-3">
-      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full"/>
+      <Image
+        src={author.profileImageUrl}
+        className="h-14 w-14 rounded-full"
+        alt={`@${author.username}'s profile picture`}
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
-        <div className="flex text-slate-400">
+        <div className="flex gap-1 text-slate-400">
           <span>
             {`@${author.username}`}
+          </span>
+          <span className="font-thin">
+            {`. ${dayjs(post.createdAt).fromNow()}`}
           </span>
         </div>
         <span>{post.content}</span>
@@ -45,7 +61,7 @@ const Home: NextPage = () => {
   const user = useUser();
   const { data, isLoading } = api.posts.getAll.useQuery();
   
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <LoadingPage />
   if (!data) return <div>Something went wrong</div>
 
   return (
