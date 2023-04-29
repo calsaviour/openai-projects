62deb4e2044727b54945a07d9a7ca50b0b4ae8c2
Calvin Low <calsaviour@gmail.com>
2023-04-29 11:44:41 +0800

    Add putObject to S3 bucket

--- icon-generator-course/src/server/api/routers/generate.ts
+++ icon-generator-course/src/server/api/routers/generate.ts
@@ -6,6 +6,15 @@ import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { Configuration, OpenAIApi } from "openai";
 import { env } from "~/env.mjs";
 import { b64Image } from "~/data/b64Image";
+import AWS from "aws-sdk";
+
+const s3 = new AWS.S3({
+  credentials: {
+    accessKeyId: env.ACCESS_KEY_ID,
+    secretAccessKey: env.SECRET_ACCESS_KEY
+  },
+  region: "us-east-1"
+})
 
 const configuration = new Configuration({
   apiKey: env.DALLE_API_KEY
@@ -63,13 +72,20 @@ export const generateRouter = createTRPCRouter({
       }
 
       // TODO: make a fetch request to DALLE api
-      const base64encoded = await generateIcon(input.prompt);
+      const base64EncodedImage = await generateIcon(input.prompt);
       
       // TODO: save images to S3 bucket
-
+      await s3.putObject({
+        Bucket: 'icon-generator-course-2',
+        Body: Buffer.from(base64EncodedImage!, "base64"),
+        Key: 'my-imageb',
+        ContentEncoding: "base64",
+        ContentType: "image/gif"
+      })
+      .promise()
 
       return {
-        imageUrl: base64encoded,
+        imageUrl: base64EncodedImage,
       };
     }),
 });
