eaa6de0842ba1e35046044aa9eb24d11e1f93776
Calvin Low <calsaviour@gmail.com>
2023-05-10 08:01:59 +0800

    Add Header and PrimaryLink component

---
+++ icon-generator-course/src/components/Header.tsx
@@ -0,0 +1,42 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { Button } from "./Button";
+import { PrimaryLink } from "./PrimaryLink";
+
+export function Header() {
+    const session = useSession();
+    const isLoggedIn = !!session.data;
+
+    return (
+        <header className="dark:bg-gray-800 flex justify-between h-16 items-center container mx-auto px-4 sm:px-0">
+            <PrimaryLink href="/">
+                Icon Generator
+            </PrimaryLink>
+            <ul>
+                <li>
+                    <PrimaryLink href="/generate">Generate</PrimaryLink>
+                </li>
+            </ul>
+            <ul>
+                {isLoggedIn && (
+                    <li>
+                        <Button onClick={() => {
+                            signOut().catch(console.error)
+                        }}>
+                            Logout
+                        </Button>
+                    </li>
+                )}
+                {!isLoggedIn && (
+                    <li>
+                    <Button onClick={() => {
+                        signIn().catch(console.error)
+                    }}>
+                        Login
+                    </Button>
+                </li>
+                )}
+            </ul>
+        </header>
+    )
+}
